eb8d91e73b6c6b19e7ada56dbdb58f1301ff83ba
Giovanni <giovanni.diana511@gmail.com>
2020-04-22 19:26:40 +0200

    Metodo Ajax

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Charts</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    </head>
+    <body>
+
+        <div class="container">
+            <canvas id="line-chart"></canvas>
+        </div>
+
+        <script src="js/main.js" charset="utf-8"></script>
+    </body>
+</html>
